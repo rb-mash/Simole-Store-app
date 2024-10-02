bd656731d578a656b81236174d741f902a8e16f7
Farai Mashonganyika <104810664+rb-mash@users.noreply.github.com>
2024-10-02 07:04:56 +0200

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,18 @@
-private void Button_Click(object sender, RoutedEventArgs e)
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace QuickApp
 {
-    textBlock.Text = "Button Clicked!";
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = "Button Clicked!";
+        }
+    }
 }
